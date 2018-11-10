256e1ea7c65acb29fe914668bc7fa847a18ef590
Ivan Yordanov <jordanov.ivan@gmail.com>
2018-11-10 19:16:38 +0400

    Add phoneList for the demo purposes

---
+++ phoneis_api/src/config/phonesList.js
@@ -0,0 +1,15 @@
+// This is only for demo purposes
+
+module.exports = {
+  phones: [
+    'Samsung Galaxy S9',
+    'Samsung Galaxy S8',
+    'Samsung Galaxy S7',
+    'Motorola Nexus 6',
+    'LG Nexus 5X',
+    'Apple iPhone X',
+    'Apple iPhone 8',
+    'Apple iPhone 4s',
+    'Apple iPhone 7',
+    'Nokia 3310'],
+};
