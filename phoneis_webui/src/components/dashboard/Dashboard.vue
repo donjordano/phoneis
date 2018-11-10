d53474d6d08cc132eb45c43e6385f9c51eaa1820
Ivan Yordanov <jordanov.ivan@gmail.com>
2018-11-11 00:07:02 +0400

    Dashboard implementation

---
+++ phoneis_webui/src/components/dashboard/Dashboard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="dashboard">
+    <h1>That's the dashboard!</h1>
+    <p>You should only get here if you're authenticated!</p>
+    <p v-if="username">Wellcome {{ username }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      username () {
+        return !this.$store.getters.user ? false : this.$store.getters.user.username
+      }
+    },
+    created () {
+      this.$store.dispatch('fetchUser')
+    }
+  }
+</script>
+
+<style scoped>
+  h1, p {
+    text-align: center;
+  }
+
+  p {
+    color: red;
+  }
+</style>
