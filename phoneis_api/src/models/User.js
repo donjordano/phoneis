5886975654cdc0f0ab7a1f3620e375a61a939604
Ivan Yordanov <jordanov.ivan@gmail.com>
2018-11-11 02:22:01 +0400

    User model refactoring

--- phoneis_api/src/models/User.js
+++ phoneis_api/src/models/User.js
@@ -7,6 +7,8 @@ const UserSchema = new Schema({
   id: ObjectId,
   firstName: String,
   lastName: String,
+  email: String,
+  password: String,
 });
 
 const User = mongoose.model('users', UserSchema);
