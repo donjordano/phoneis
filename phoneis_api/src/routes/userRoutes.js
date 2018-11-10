37d69e184b9b17933cac4eab8df0483d0821a7ea
Ivan Yordanov <jordanov.ivan@gmail.com>
2018-11-11 02:24:51 +0400

    Create routers to handle auth

---
+++ phoneis_api/src/routes/userRoutes.js
@@ -0,0 +1,16 @@
+const { Router } = require('restify-router');
+const { ensureAuthenticated } = require('../utils');
+
+const router = new Router();
+const UserController = require('../controllers/userController.js');
+
+router.post('/', UserController.createUser);
+
+// Protected endpoints
+router.get('/:id', UserController.getUser);
+router.get('/all', ensureAuthenticated, UserController.listAllUsers);
+
+// Authentication mechanism
+router.post('/auth', UserController.auth);
+
+module.exports = router;
