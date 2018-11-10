898aec207d27dbf7ac84937c820befed4dc27174
Ivan Yordanov <jordanov.ivan@gmail.com>
2018-11-11 00:07:51 +0400

    main.js implementation

--- phoneis_webui/src/main.js
+++ phoneis_webui/src/main.js
@@ -1,7 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
+import router from './router/routes'
+import store from './store/store'
+
+axios.defaults.baseURL = ''
+
+// axios.defaults.headers.common['Authorization'] = 'xxx-XXX-xxx-XXX-xxx'
+axios.defaults.headers.get['Accepts'] = 'application/json'
+
+const reqInterceptor = axios.interceptors.request.use(config => {
+  return config
+})
+const resInterceptor = axios.interceptors.response.use(res => {
+  return res
+})
+
+axios.interceptors.request.eject(reqInterceptor)
+axios.interceptors.response.eject(resInterceptor)
 
 new Vue({
   el: '#app',
+  router,
+  store,
   render: h => h(App)
 })
