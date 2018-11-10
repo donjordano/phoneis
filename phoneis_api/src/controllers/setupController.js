b53830c6671273de4476984d86c95fcd19b52405
Ivan Yordanov <jordanov.ivan@gmail.com>
2018-11-10 21:01:12 +0400

    very basic auth implementation

--- phoneis_api/src/controllers/setupController.js
+++ phoneis_api/src/controllers/setupController.js
@@ -3,4 +3,27 @@ module.exports = (server, plugins, restifyValidator) => {
   server.use(plugins.queryParser());
   server.use(plugins.bodyParser());
   server.use(restifyValidator);
+
+  // Basic auth implementation
+  server.use(plugins.authorizationParser());
+  server.use((req, res, next) => {
+    const apiKeys = {
+      apiUser: 'xxx-XXX-xxx-XXX-xxx',
+    };
+
+    if (typeof (req.authorization.basic) === 'undefined'
+      || !apiKeys[req.authorization.basic.username]
+      || apiKeys[req.authorization.basic.username] !== req.authorization.basic.password) {
+      const response = {
+        status: 'failure',
+        data: 'Please specify a valid API key',
+      };
+      res.setHeader('content-type', 'application/json');
+      res.writeHead(403);
+      res.end(JSON.stringify(response));
+      return next();
+    }
+
+    return next();
+  });
 };
