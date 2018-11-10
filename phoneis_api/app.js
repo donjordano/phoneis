0ddc1aa7e82aac81c6e7be131d90c47f8e688c41
Ivan Yordanov <jordanov.ivan@gmail.com>
2018-11-10 15:47:42 +0400

    Implement app.js

---
+++ phoneis_api/app.js
@@ -0,0 +1,28 @@
+// Libraries requires
+const restify = require('restify');
+const plugins = require('restify-plugins');
+const restifyValidator = require('restify-validator');
+const mongoose = require('mongoose');
+
+// Create Restify Server
+const server = restify.createServer();
+
+//  DB conenction
+const dbConnection = require('./src/config/dbConnection');
+
+// Connect to mongo
+mongoose.connect(dbConnection.getMongoConnection(), { useNewUrlParser: true });
+
+// Controllers
+const setupController = require('./src/controllers/setupController');
+const userController = require('./src/controllers/userController');
+const phoneController = require('./src/controllers/phoneController');
+// Setup controllers
+setupController(server, plugins, restifyValidator);
+userController(server);
+phoneController(server);
+
+// Start server listener
+server.listen(1337, () => {
+  console.log('%s listening at %s', server.name, server.url);
+});
